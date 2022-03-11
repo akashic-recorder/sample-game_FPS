20cabe3d5346f040f8e7182d4cedb0d49691afb4
hideki.koike <hideki.koike@tx-inc.com>
2022-03-12 03:59:12 +0900

    add log

--- Assets/Scripts/AkashicApiConnector.cs
+++ Assets/Scripts/AkashicApiConnector.cs
@@ -1,7 +1,19 @@
-﻿using UnityEngine;
+﻿using Unity.FPS.Game;
+using UnityEngine;
 
-
+[RequireComponent(typeof(Health))]
 public class AkashicApiConnector : MonoBehaviour
 {
+    Health _health;
     
+    void Start()
+    {
+        _health = GetComponent<Health>();
+        _health.OnDie += ConnectApi;
+    }
+
+    void ConnectApi()
+    {
+        Debug.Log($"connect wallet and call API");
+    }
 }
