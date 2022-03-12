5004d4086d337405db05c6bd69e99bf3b6fcce0f
hideki.koike <hideki.koike@tx-inc.com>
2022-03-12 23:55:24 +0900

    add browser opener

---
+++ Assets/AkashicRecorder/Scripts/Entities/BrowserOpener.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public sealed class BrowserOpener : MonoBehaviour
+{
+    [SerializeField] string url = "https://www.google.com";
+    
+    public void OpenBrowser()
+    {
+        Application.OpenURL(url);
+    }
+}
